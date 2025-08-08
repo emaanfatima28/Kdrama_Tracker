023f63de413fabd025998e8b8b83e03e8c7c475d
Emaan Fatima <emaanfatima0613@gmail.com>
2025-08-08 11:22:53 +0500

    Deployment Issues Resolved

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use('/api/dramas', dramaRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "frontend", "dist"))); 
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); 
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
